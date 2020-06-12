b6081556554c74811b2e4536385a7ada3cb169db
xofl <xofl@ilscp.net>
2020-06-12 15:13:35 +0900

    init

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -5,13 +5,13 @@ APP_HOST=${APP_HOST:-app}
 APP_PORT_NUMBER=${APP_PORT_NUMBER:-8000}
 
 # Check if SSL should be enabled (if certificates exists)
-if [ -f "/cert/cert.pem" -a -f "/cert/key-no-password.pem" ]; then
+if [ -f "/cert/nginx_ssl.crt" -a -f "/cert/_wildcard_ilscp_net_SHA256WITHRSA.key" ]; then
   echo "found certificate and key, linking ssl config"
   ssl="-ssl"
 else
   echo "linking plain config"
 fi
-# Ensure that the configuration file is not present before linking. 
+# Ensure that the configuration file is not present before linking.
 test -w /etc/nginx/conf.d/mattermost.conf && rm /etc/nginx/conf.d/mattermost.conf
 # Linking Nginx configuration file
 ln -s -f /etc/nginx/sites-available/mattermost$ssl /etc/nginx/conf.d/mattermost.conf
